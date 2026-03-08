6528967ffaa36c36c1f014b95e3283f69457449f
Qasim-Ali252 <qasimmunir633@gmail.com>
2026-03-08 19:46:42 +0500

    Add utility modules

---
+++ utils/logger.py
@@ -0,0 +1,19 @@
+import logging
+import sys
+
+def setup_logger():
+    """Setup logging configuration"""
+    logger = logging.getLogger('KD6')
+    logger.setLevel(logging.INFO)
+    
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.INFO)
+    
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        datefmt='%H:%M:%S'
+    )
+    handler.setFormatter(formatter)
+    
+    logger.addHandler(handler)
+    return logger
